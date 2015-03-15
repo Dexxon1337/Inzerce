393399a6e0013c61977852e2a57ea2a804838507
DexxonPlugins <batfameq@seznam.cz>
2015-03-15 14:55:51 +0100

    Signed-off-by: DexxonPlugins <batfameq@seznam.cz>

--- src/me/capanddex/ConfigHandler.java
+++ src/me/capanddex/ConfigHandler.java
@@ -4,17 +4,19 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public class ConfigHandler {
-	private Player inzerent;
+/*	private Player inzerent;
 	private String inzerat;
 	private FileConfiguration soubor;
-	public void ulozInzerat() {
+	*/
+	public static void ulozInzerat(Player inzerent, String inzerat, FileConfiguration soubor) {
 		soubor.set("inzeraty.hrac", inzerent.getName());
 		soubor.set("inzeraty.hrac.inzerat", inzerat);
 	}
-	ConfigHandler(Player player, String text, FileConfiguration config) {
+/*	ConfigHandler(Player player, String text, FileConfiguration config) {
 		inzerent = player;
 		inzerat = text;
 		soubor = config;
 	}
+	*/
 
 }
