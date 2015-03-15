8eb8cd816519f3ce49d7a63c47764b3aba9a47e7
DexxonPlugins <batfameq@seznam.cz>
2015-03-15 13:11:13 +0100

    Signed-off-by: DexxonPlugins <batfameq@seznam.cz>

---
+++ src/me/capanddex/Inzerce.java
@@ -0,0 +1,26 @@
+package me.capanddex;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Inzerce extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	@Override
+	public void onDisable() {
+		
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("inzerat") && sender instanceof Player) {
+			Player player = (Player) sender;
+			
+//Capitano má malý dick			
+			return true;
+		}
+		return false;
+	}
+}
