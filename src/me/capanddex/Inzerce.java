20408f2318319b50849ade54c25590f8e04a940b
DexxonPlugins <Vas4you@seznam.cz>
2015-03-23 00:07:01 +0100

    item transfer + payment

--- src/me/capanddex/Inzerce.java
+++ src/me/capanddex/Inzerce.java
@@ -1,6 +1,11 @@
 package me.capanddex;
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
@@ -11,18 +16,35 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+
 public class Inzerce extends JavaPlugin {
+    private static final Logger log = Logger.getLogger("Minecraft");
+    public static Economy econ = null;
+    public static Permission perms = null;
+    public static Chat chat = null;
+    
 	private ArrayList<Inzerat> list;
 	private int counter = 1;
-
+	
 	@Override
 	public void onEnable() {
+		  if (!setupEconomy() ) {
+	            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+	            getServer().getPluginManager().disablePlugin(this);
+	            return;
+	        }
+	        setupPermissions();
+	        setupChat();
 		new LogoutListener(this);
 		getLogger().info("Plugin spusten.");
 		list = new ArrayList<Inzerat>();
+		
 	}
+
 
 	@Override
 	public void onDisable() {
@@ -46,14 +68,14 @@ public class Inzerce extends JavaPlugin {
 						return true;
 					}
 				}
-				else if (args[0].equalsIgnoreCase("odpovedet")) {
+				else if (args[0].equalsIgnoreCase("odp") || args[0].equalsIgnoreCase("odpoved") ||  args[0].equalsIgnoreCase("odpovedet")) {
 					answer(player, args, list);
 				}
 				
 				else if (args[0].equalsIgnoreCase("help")) {
 					help(player);
 				}
-				else if (args[0].equalsIgnoreCase("vypsat")) {
+				else if (args[0].equalsIgnoreCase("seznam") || args[0].equalsIgnoreCase("list")) {
 					vypsat(player, list);
 				}
 
@@ -62,12 +84,6 @@ public class Inzerce extends JavaPlugin {
 				smazat(player, args, list);
 				}
 
-			/*
-			 * else if (args[0].equalsIgnoreCase("reload")) { // plugin //
-			 * disable if (player.hasPermission("inzerat.admin")) {
-			 * this.setEnabled(false);
-			 * player.sendMessage("§3Plugin §cReloaded!"); } }
-			 */
 				else if (args[0].equalsIgnoreCase("fake")) {
 					if (player.hasPermission("inzerat.admin")) {
 						addToList(player, list);
@@ -117,7 +133,7 @@ public class Inzerce extends JavaPlugin {
 		return true;
 	}
 
-	// help do konzole
+	// help in console
 	public static void help(CommandSender sender) {
 		sender.sendMessage("§3[********************§6Plugin §lInzerce §6Help§3********************]");
 		sender.sendMessage("§a/inzerat help §b- zobrazí tuto nápovedu.");
@@ -129,8 +145,7 @@ public class Inzerce extends JavaPlugin {
 		sender.sendMessage("§3[********************§6Plugin §lInzerce §6Help§3********************]");
 
 	}
-
-	// help pro hrace
+	// help for players
 	public static void help(Player player) {
 		if (player.hasPermission("inzerat.admin")) {
 			player.sendMessage("§3[********************§6Plugin §lInzerce §6Help§3********************]");
@@ -151,7 +166,7 @@ public class Inzerce extends JavaPlugin {
 		}
 
 	}
-//create a SELL announcement
+	//create a SELL announcement
 	public static String prodam(Player player, String[] args, int count,
 			ArrayList<Inzerat> list) {
 		double price = 0;
@@ -214,7 +229,7 @@ public class Inzerce extends JavaPlugin {
 
 	
 	}
-//delete an announcement
+	//delete an announcement
 	public static void smazat(Player player, String[] args,
 			ArrayList<Inzerat> list) {
 		if (player.hasPermission("inzerat.admin")) {
@@ -266,7 +281,7 @@ public class Inzerce extends JavaPlugin {
 			}
 		}
 	}
-//print the list
+	//print the list
 	public static void vypsat(Player player, ArrayList<Inzerat> list) {
 
 		player.sendMessage(ChatColor.AQUA + "---Výpis všech " + list.size()
@@ -282,7 +297,7 @@ public class Inzerce extends JavaPlugin {
 		}
 		player.sendMessage(ChatColor.AQUA + "---Konec seznamu---");
 	}
-//add a testing instance to list
+	//add a testing instance to the list
 	public static void addToList(Player player, ArrayList<Inzerat> list) {
 		Inzerat ad = new Inzerat(0, player, Material.AIR, 1, 1, false);
 		player.sendMessage(ChatColor.GREEN + "Pøidávám testovací inzerát");
@@ -306,6 +321,11 @@ public class Inzerce extends JavaPlugin {
 		}
 
 	}
+	//answer an advertisement
+	
+	
+	
+	
 	public static void answer(Player player, String [] args, ArrayList<Inzerat> list) {
 		if(args.length>=1 && StringUtils.isNumeric(args[1])) {
 			int id = Integer.parseInt(args[1]);
@@ -320,16 +340,34 @@ public class Inzerce extends JavaPlugin {
 				if(currentAd.isBuy()) {
 					
 				}
-			//	if(player má prachy) {
+			
 				else {
+			if(econ.getBalance(player)>=currentAd.getPrice()) {	
 				if(hasItem(currentAd.getAdvertiser(),currentAd.getMat(),currentAd.getCount())) {
+					int count = 0;
+					for (ItemStack i : player.getInventory()) {
+						if (i == null) {
+							count++;
+						}
+					}
+					if(count>=Math.ceil(currentAd.getCount()/64)) {
+						currentAd.getAdvertiser().getInventory().removeItem(new ItemStack[] {new ItemStack(currentAd.getMat(), currentAd.getCount()) });
+						player.getInventory().addItem(new ItemStack(currentAd.getMat(),currentAd.getCount()));
+					econ.withdrawPlayer(player, currentAd.getPrice());
+					econ.depositPlayer(currentAd.getAdvertiser(), currentAd.getPrice());
+
 					currentAd.getAdvertiser().sendMessage(ChatColor.GREEN + "Prodej §a" + currentAd.getCount() + " " + currentAd.getMat().toString().toLowerCase().replace("_", " ") +  "hráèi §a" + player.getDisplayName() + " byl úspìšný!");
-					player.sendMessage(ChatColor.GREEN + "Nákup " + currentAd.getCount() + " " + currentAd.getMat().toString().replace("_", " ") + " od hráèe " + currentAd.getAdvertiser().getDisplayName() + " byl úspìšný!");
-					
+					player.sendMessage(ChatColor.GREEN + "Nákup §a" + currentAd.getCount() + " " + currentAd.getMat().toString().replace("_", " ") + " od hráèe " + currentAd.getAdvertiser().getDisplayName() + " byl úspìšný!");
+				
 					for(int i = list.size()-1; i>=0;i--) {
 						if(list.get(i).getID()==id) {
 							list.remove(i);
 						}
+					}
+					
+					}
+					else {
+						player.sendMessage(ChatColor.RED + "Pro nákup tohoto poètu vìcí potøebuješ alespoò" + Math.ceil(currentAd.getCount()/64) + "místa v inventáøi.");
 					}
 				}
 				else {
@@ -337,7 +375,7 @@ public class Inzerce extends JavaPlugin {
 					currentAd.getAdvertiser().sendMessage("§aPotøebný poèet: §c" + currentAd.getCount());
 					player.sendMessage("§aHráè §c" + currentAd.getAdvertiser().getDisplayName() + "§a nemá dostatek itemu na prodej. Zkus ho kontaktovat pomocí /msg §c" + currentAd.getAdvertiser().getName());
 				}
-		//	}
+			}
 			}
 			}
 			else {
@@ -354,7 +392,7 @@ public class Inzerce extends JavaPlugin {
 
 		}
 	}
-	
+	//check if number of items is right
     public static boolean overitPocet(Player player, ArrayList<Inzerat> list) {
     	int counter = 0;
     	for(int i = 0; i < list.size(); i++) {
@@ -373,5 +411,32 @@ public class Inzerce extends JavaPlugin {
 	public ArrayList<Inzerat> getList() {
 		return this.list;
 	}
+	
+	
+	
+	
+	//Vault linking
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
 }
